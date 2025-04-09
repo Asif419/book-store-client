8cbde8cf7e1e6fbf85af73303399193c4f4e542e
Md. Asif Shahariar <asshah419@gmail.com>
2025-04-09 19:02:27 +0300

    footer, testimonials added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,9 @@
 import Banner from "../component/ui/HomePage/Banner";
 import FeaturedProducts from "../component/ui/HomePage/FeaturedProducts";
+import Footer from "../component/ui/HomePage/Footer";
 import Hero from "../component/ui/HomePage/Hero";
 import Navbar from "../component/ui/HomePage/Navbar";
+import Testimonials from "../component/ui/HomePage/Testimonials";
 
 const HomePage = () => {
   return (
@@ -19,11 +21,11 @@ const HomePage = () => {
       {/* Featured Books */}
       <FeaturedProducts></FeaturedProducts>
 
+      {/* Testimonials */}
+      <Testimonials></Testimonials>
+
       {/* Footer */}
-      <footer className="bg-base-200 text-center py-4 text-sm">
-        &copy; {new Date().getFullYear()} Book Shop. All rights reserved.
-      </footer>
-      <p className="text-center text-sm text-gray-500">Made with ❤️ by Your Team</p>
+      <Footer></Footer>
     </div>
   );
 };
