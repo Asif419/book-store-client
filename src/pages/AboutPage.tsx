768a098771b404050831779c8b922c9c2333f2ac
Md. Asif Shahariar <asshah419@gmail.com>
2025-05-14 17:41:24 +0300

    dark mode removed

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -7,11 +7,11 @@ const AboutPage = () => {
   }, []);
   return (
     <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-1 md:py-4 lg:py-12 space-y-16">
-      <h2 className="text-xl md:text-2xl lg:text-3xl font-bold text-center mb-10 text-primary">About Us</h2>
+      <h2 className="text-3xl font-bold text-center mb-10 text-primary mt-8 md:mt-0">About Us</h2>
       {/* Section 1 */}
       <div className="flex flex-col md:flex-row items-center gap-8 bg-base-200 rounded-lg">
         <div className="flex-1">
-          <h2 className="text-3xl font-bold mb-4">About BookShop</h2>
+          <h2 className="text-2xl font-bold mb-4">About BookShop</h2>
           <p className="text-gray-700 text-lg leading-relaxed">
             BookShop was built by a group of passionate readers, developers, and designers with one mission: to make discovering great books easier than ever. We aim to empower curious minds by providing easy access to the stories and ideas that shape the world.
           </p>
@@ -35,7 +35,7 @@ const AboutPage = () => {
           />
         </div>
         <div className="flex-1">
-          <h2 className="text-3xl font-bold mb-4">Discover in Moments</h2>
+          <h2 className="text-2xl font-bold mb-4">Discover in Moments</h2>
           <p className="text-gray-700 text-lg leading-relaxed">
             We know time is precious. That’s why BookShop helps you find powerful reads without wasting hours browsing. With curated collections and smart discovery tools, we turn idle moments into opportunities to learn, escape, and grow.
           </p>
